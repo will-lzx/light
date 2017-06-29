369d9ee4f94e07f1a629112709d675f44d72c3b7
will-lzx <will-liu@live.com>
2017-06-29 14:24:39 +0800

    add token

--- django_wsgi.py
+++ django_wsgi.py
@@ -4,10 +4,6 @@
 import os
 import sys
 
-
-reload(sys)
-sys.setdefaultencoding('utf8')
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "light.settings")
 
 from django.core.wsgi import get_wsgi_application
