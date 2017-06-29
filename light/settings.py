c27696ff0c8e5d5fd1bdb00ba43290a60cb1c7cb
will-lzx <will-liu@live.com>
2017-06-29 14:00:35 +0800

    add token

--- light/settings.py
+++ light/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'home',
-    'weixin',
+    # 'weixin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
