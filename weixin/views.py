919633008f2184d937048eb09997d19e97308bef
will-lzx <will-liu@live.com>
2017-07-25 17:24:55 +0800

    reply message

--- weixin/views.py
+++ weixin/views.py
@@ -199,7 +199,6 @@ def wxconfig(request):
 def wx(request):
     if request.method == 'GET':
         signature = request.GET.get('signature', None)
-
         timestamp = request.GET.get('timestamp', None)
         nonce = request.GET.get('nonce', None)
         echostr = request.GET.get('echostr', None)
@@ -207,7 +206,7 @@ def wx(request):
         tmpList = [token, timestamp, nonce]
         tmpList.sort()
         tmpstr = "%s%s%s" % tuple(tmpList)
-        tmpstr = hashlib.sha1(tmpstr).hexdigest()
+        tmpstr = hashlib.sha1(tmpstr.encode('utf-8')).hexdigest()
         if tmpstr == signature:
             return HttpResponse(echostr)
         else:
