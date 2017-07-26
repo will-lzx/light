80cf84c0ddf5ade2d58bca2cea9528c50cdef61f
will-lzx <will-liu@live.com>
2017-07-26 17:00:04 +0800

    reply message

--- weixin/views.py
+++ weixin/views.py
@@ -233,6 +233,8 @@ def oauth_user(request):
     print('oauth_url', oauth_url)
 
     req = urllib.request.Request(oauth_url)
+    req.add_header('Content-Type', 'application/json')
+    req.add_header('encoding', 'utf-8')
     res = urllib.request.urlopen(req)
     urlResp = json.loads(res.read())
 
