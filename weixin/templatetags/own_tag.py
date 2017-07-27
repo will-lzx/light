7aade5f1b900a9b9e736f828757e9bb3dfe040c4
will-lzx <will-liu@live.com>
2017-07-27 14:06:58 +0800

    fix the privatecenter

--- weixin/templatetags/own_tag.py
+++ weixin/templatetags/own_tag.py
@@ -5,9 +5,9 @@ register = template.Library()
 
 
 def datetime_format(timestamp):
-    timeArray = time.localtime(timestamp)
+    now = datetime.datetime.now()
 
-    time_str = time.strftime("%Y-%m-%d %H:%M:%S", timeArray + datetime.timedelta(hours=8))
+    time_str = time.strftime("%Y-%m-%d %H:%M:%S", now + datetime.timedelta(hours=8))
     return time_str
 
 register.filter('datetime_format', datetime_format)
