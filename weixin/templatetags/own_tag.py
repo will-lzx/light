202f9a184635abd0555607a70ce401156d56601c
will-lzx <will-liu@live.com>
2017-07-27 14:05:45 +0800

    fix the privatecenter

--- weixin/templatetags/own_tag.py
+++ weixin/templatetags/own_tag.py
@@ -1,11 +1,13 @@
 import time
+import datetime
 from django import template
 register = template.Library()
 
 
 def datetime_format(timestamp):
     timeArray = time.localtime(timestamp)
-    time_str = time.strftime("%Y-%m-%d %H:%M:%S", timeArray)
+
+    time_str = time.strftime("%Y-%m-%d %H:%M:%S", timeArray + datetime.timedelta(hours=8))
     return time_str
 
 register.filter('datetime_format', datetime_format)
