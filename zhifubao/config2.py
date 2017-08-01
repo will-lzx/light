ddb0a7ab99cfe9b85f6aa155032b5ab722336e0d
will-lzx <will-liu@live.com>
2017-08-01 16:24:03 +0800

    add gateway

--- zhifubao/config2.py
+++ zhifubao/config2.py
@@ -10,7 +10,7 @@ class settings:
     # 签约支付宝账号或卖家支付宝帐户
     ALIPAY_SELLER_EMAIL = '470235676@qq.com'
 
-    ALIPAY_SIGN_TYPE = 'MD5'
+    ALIPAY_SIGN_TYPE = 'RSA2'
 
     # 付完款后跳转的页面（同步通知） 要用 http://格式的完整路径，不允许加?id=123这类自定义参数
     ALIPAY_RETURN_URL = 'http://relalive.com/zhifubao/zfb/'
