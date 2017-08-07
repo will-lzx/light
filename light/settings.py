1f4ef344e79755d68fb60f4c1afb89a371444f14
will-lzx <will-liu@live.com>
2017-08-07 14:38:29 +0800

    save lendhistory

--- light/settings.py
+++ light/settings.py
@@ -113,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'zh_hans'
+LANGUAGE_CODE = 'zh-Hans'
 # DEFAULT_CHARSET = 'GB2312'
 
 TIME_ZONE = 'UTC'
