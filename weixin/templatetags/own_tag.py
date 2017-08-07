58e94fde0a35a52014b6f54e9cc2ec6ba3d4caa0
will-lzx <will-liu@live.com>
2017-08-07 12:04:46 +0800

    save lendhistory

--- weixin/templatetags/own_tag.py
+++ weixin/templatetags/own_tag.py
@@ -15,9 +15,9 @@ def get_time_long(start_time, return_time=None):
     if return_time:
         return return_time - start_time
     else:
-        print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-        print(start_time)
-        return datetime.datetime.now() - start_time
+        now_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + datetime.timedelta(hours=8))
+
+        return now_time - start_time
 
 
 def convert_time(old_time):
