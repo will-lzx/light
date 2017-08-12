0413cd3750d150e48b3f60c3790b9119b591223f
will-lzx <will-liu@live.com>
2017-08-13 06:46:05 +0800

    get user_id

--- zhifubao/views.py
+++ zhifubao/views.py
@@ -22,7 +22,7 @@ from lib.utils.common import *
 
 
 def lend(request):
-    user_id = get_user_id(request)
+    #user_id = get_user_id(request)
     # print('user_id:', user_id)
     template_name = 'zhifubao/lend.html'
     print('test:')
