22636d0b2f88083915ab060c60ab8506a6e191a7
will-lzx <will-liu@live.com>
2017-08-12 16:41:00 +0800

    get user_id

--- lib/utils/common.py
+++ lib/utils/common.py
@@ -61,7 +61,7 @@ def get_userid(code):
     message = "&".join(u"{}={}".format(k, v) for k, v in unsigned_items)
     key = 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAhug73b3e2juIOIfxN7Ju2AMcwdOVqG4txOCea+r6nQBMyrlEIIbi1gKWFIbTCJAeKRhJPAZnApd8CCPGwSgRyxbYAUxJNF4BTIECTIHc0nXZVJASv6L0Miqnv7G2X1PFSWMlt4ijmo0f3mCnZONbk8MKcesSSN0EV5WfyJA/PUs+4rbJrEwCnoEtR6TgX+JPg+oa03/718T3jJGz4saWRH7QJD+jPFluZusy2LEMmckX+ZPusSpGZdEunqxbCoM8ywN+Ag2h9L6qOdj1VMTlzu/vweRyZDBW2ztWelbuzW7JRPrIGce0X0vomJ1ATEIPuCidaP16V6K+sguWsgNe8wIDAQAB'
     signer = PKCS1_v1_5.new(key)
-    digest = SHA256.new(message)
+    digest = SHA256.new(message.encode(encoding='utf-8'))
     sign_str = signer.sign(digest)
 
     print('sign_str', sign_str)
