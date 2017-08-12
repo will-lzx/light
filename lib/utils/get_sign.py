caf16e35624ee1739c0839226055fdb3d08f8bdc
will-lzx <will-liu@live.com>
2017-08-12 15:57:41 +0800

    get user_id

--- lib/utils/get_sign.py
+++ lib/utils/get_sign.py
@@ -6,7 +6,7 @@ from cryptography.hazmat.primitives.asymmetric import padding
 
 
 # 签名函数
-def sign(data):
+def sign(data, private_key_file_name='/root/zhifubao/app_private_key'):
     """
     签名函数使用指定的公钥Key对文件进行签名，并将签名结果写入文件中
     :param data: 待签名的数据文件
@@ -16,9 +16,14 @@ def sign(data):
     # 从PEM文件中读取私钥数据
     key_data = 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAhug73b3e2juIOIfxN7Ju2AMcwdOVqG4txOCea+r6nQBMyrlEIIbi1gKWFIbTCJAeKRhJPAZnApd8CCPGwSgRyxbYAUxJNF4BTIECTIHc0nXZVJASv6L0Miqnv7G2X1PFSWMlt4ijmo0f3mCnZONbk8MKcesSSN0EV5WfyJA/PUs+4rbJrEwCnoEtR6TgX+JPg+oa03/718T3jJGz4saWRH7QJD+jPFluZusy2LEMmckX+ZPusSpGZdEunqxbCoM8ywN+Ag2h9L6qOdj1VMTlzu/vweRyZDBW2ztWelbuzW7JRPrIGce0X0vomJ1ATEIPuCidaP16V6K+sguWsgNe8wIDAQAB'
 
+    key_file = open(private_key_file_name, 'rb')
+    key_data = key_file.read()
+    key_file.close()
+
     # 从PEM文件数据中加载公钥
-    private_key = serialization.load_pem_public_key(
+    private_key = serialization.load_pem_private_key(
         key_data,
+        password=None,
         backend=default_backend()
     )
 
