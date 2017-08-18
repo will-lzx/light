8943216f206cc89846ce83b68a935d113b85c319
will-lzx <will-liu@live.com>
2017-08-18 16:30:43 +0800

    merge weixin and zhifubao

--- static/js/relalive.js
+++ static/js/relalive.js
@@ -4,3 +4,26 @@ function showTip(tip, type) {
     $tip.attr('class', 'alert alert-' + type).text(tip).css('margin-left', - $tip.outerWidth() / 2).fadeIn(500).delay(2000).fadeOut(500);
 }
 
+function jsApiCall()
+{
+    WeixinJSBridge.invoke(
+    'getBrandWCPayRequest', {{ data | safe }},
+    function(res){
+      window.location.href="{{ redirect_uri }}";
+      }
+    );
+}
+
+function callpay()
+{
+    if (typeof WeixinJSBridge == "undefined"){
+        if( document.addEventListener ){
+            document.addEventListener('WeixinJSBridgeReady', jsApiCall, false);
+        }else if (document.attachEvent){
+            document.attachEvent('WeixinJSBridgeReady', jsApiCall);
+            document.attachEvent('onWeixinJSBridgeReady', jsApiCall);
+        }
+    }else{
+        jsApiCall();
+    }
+}
